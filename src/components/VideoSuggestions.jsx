18ad285aaebabaf6baa2fd54d994d41e04860d7a
is6ak <39702@ma-web.nl>
2026-03-23 12:14:09 +0100

    videoSuggestion somewhat done

--- src/components/VideoSuggestions.jsx
+++ src/components/VideoSuggestions.jsx
@@ -1,9 +1,17 @@
 import styles from './videoSuggestions.module.css';
+import imgHolder from '../assets/imgHolder.jpg';
 
 export function VideoSuggestions() {
   return (
     <div className={styles.videoSuggestions}>
-      <div className={styles.videoTitle}>Title</div>
+      <div className={styles.videoSuggestion}>
+        <img src={imgHolder} className={styles.videoImg}/>
+        <div className={styles.videoDescription}>
+          <div className={styles.videoTitle}>Title</div>
+          <div className={styles.videoUploader}>Uploader</div>
+          <div className={styles.videoLength}>00:00</div>
+        </div>
+      </div>
     </div>
   );
 };
