089e19a21cda5041e881201c1da7ab81bf584bfc
is6ak <39702@ma-web.nl>
2026-03-23 12:37:52 +0100

    finish

--- src/components/VideoSuggestions.jsx
+++ src/components/VideoSuggestions.jsx
@@ -1,11 +1,18 @@
 import styles from './videoSuggestions.module.css';
 import imgHolder from '../assets/imgHolder.jpg';
+import videoFile from '../assets/Nothing But Thieves - Overcome.mp4';
+import {useVideoThumbnail} from './GenerateThumbnail'
 
 export function VideoSuggestions() {
+  const thumbnail = useVideoThumbnail(videoFile);
   return (
     <div className={styles.videoSuggestions}>
       <div className={styles.videoSuggestion}>
-        <img src={imgHolder} className={styles.videoImg}/>
+        {thumbnail ? (
+          <img src={thumbnail} className={styles.videoImg} />
+        ) : (
+          <div>Loading...</div>
+        )}
         <div className={styles.videoDescription}>
           <div className={styles.videoTitle}>Title</div>
           <div className={styles.videoUploader}>Uploader</div>
