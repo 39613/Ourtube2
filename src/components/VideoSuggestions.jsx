73c7613daf42b7184bfb784f89c21ca3b116e979
is6ak <39702@ma-web.nl>
2026-03-23 11:51:51 +0100

    finish

--- src/components/VideoSuggestions.jsx
+++ src/components/VideoSuggestions.jsx
@@ -1,15 +1,9 @@
-import styles from "./videoSuggestions.module.css";
+import styles from './videoSuggestions.module.css';
 
 export function VideoSuggestions() {
-  const video_info = [];
-
   return (
-<<<<<<< Updated upstream
-    <div className={styles.videoSuggestions}>VideoSuggestions Component</div>
-=======
     <div className={styles.videoSuggestions}>
       <div className={styles.videoTitle}>Title</div>
     </div>
->>>>>>> Stashed changes
   );
-}
+};
