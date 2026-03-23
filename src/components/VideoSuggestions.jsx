af587e5d5e9debd4f331dff7b99df642a1946cef
39613 <39613@ma-web.nl>
2026-03-23 11:50:13 +0100

    test commit

--- src/components/VideoSuggestions.jsx
+++ src/components/VideoSuggestions.jsx
@@ -1,9 +1,9 @@
-import styles from './videoSuggestions.module.css';
+import styles from "./videoSuggestions.module.css";
 
 export function VideoSuggestions() {
+  const video_info = [];
+
   return (
-    <div className={styles.videoSuggestions}>
-      VideoSuggestions Component
-    </div>
+    <div className={styles.videoSuggestions}>VideoSuggestions Component</div>
   );
-};
+}
