989165cb1922426545c87d29fdd5cfb4ccfa4cc9
39613 <39613@ma-web.nl>
2026-03-23 12:16:08 +0100

    loop code

--- src/components/VideoSuggestions.jsx
+++ src/components/VideoSuggestions.jsx
@@ -1,9 +1,18 @@
 import styles from "./videoSuggestions.module.css";
 
 export function VideoSuggestions() {
-  const video_info = [];
+  const video_info = [
+    {
+      url: "https://i.ytimg.com/vi/xJ9oQKMZOh4/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBcIuIb-RAAx351tb7M9i1cRQ4w5w",
+      title: "SIDEMEN CHARITY MATCH 2026: THE DRAFT",
+    },
+  ];
+  const suggestion = video_info.map((item) => (
+    <div className={styles.container}>
+      <img src={item.url} alt="" />
+      <p>{item.title}</p>
+    </div>
+  ));
 
-  return (
-    <div className={styles.videoSuggestions}>VideoSuggestions Component</div>
-  );
+  return <div className={styles.videoSuggestions}>{suggestion}</div>;
 }
